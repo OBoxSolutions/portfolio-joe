b708309ba8f34759ae3a4af2e5ee408386fbd1e0
Elodin <jcassola96@gmail.com>
2022-08-24 16:48:55 -0400

    make base button

---
+++ components/BaseButton.vue
@@ -0,0 +1,28 @@
+<template>
+  <button
+    class="button bg-button"
+    v-bind="$attrs"
+    v-on="$listeners"
+  >
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+.button{
+  color: #ffffff;
+  border: none;
+  outline: none;
+  border-radius: 10px;
+  padding: .2rem 1rem;
+  cursor: pointer;
+}
+
+</style>
--- components/SectionHero.vue
+++ components/SectionHero.vue
@@ -1,17 +1,23 @@
 <template>
   <div>
-    <h1>
-      Section Hero works!
-    </h1>
+    Section Hero works!
   </div>
 </template>
 
 <script>
 export default {
-  name: 'SectionHero'
+  name: 'SectionHero',
+  components: { }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+// .hero-button{
+//   font-family: 'Amiko';
+//   font-style: normal;
+//   font-weight: 400;
+//   font-size: 32px;
+//   line-height: 43px;
+// }
 
 </style>
