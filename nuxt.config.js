6ea8d9a007914025d3564fe29d0d079e1a56ee77
Elodin <jcassola96@gmail.com>
2022-08-24 15:40:15 -0400

    set scss general styles

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,8 +17,7 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: ['~/assets/scss/main.scss'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
@@ -39,5 +38,6 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    postcss: null
   }
 }
