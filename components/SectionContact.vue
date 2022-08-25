e72b556719d69d286980e03d50936296196b4ebf
Elodin <jcassola96@gmail.com>
2022-08-25 15:58:51 -0400

    make contact me section

--- components/SectionContact.vue
+++ components/SectionContact.vue
@@ -1,6 +1,23 @@
 <template>
-  <div id="contact">
-    Section Contact works!
+  <div id="contact" class="bg-primary">
+    <h1>Contact Me</h1>
+    <div class="social-networks">
+      <div class="rounded-circle bg-text-button">
+        <a href="#">
+          <img src="/linked-in.svg" alt="linked-in">
+        </a>
+      </div>
+      <div class="rounded-circle bg-text-button">
+        <a href="#">
+          <img src="/github.svg" alt="github">
+        </a>
+      </div>
+      <div class="rounded-circle bg-text-button">
+        <a href="#">
+          <img src="/email.svg" alt="email">
+        </a>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -10,6 +27,24 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+#contact{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1em;
 
+  .social-networks{
+    display: flex;
+    gap: 1em;
+    margin-bottom: 3rem;
+  }
+  .rounded-circle{
+    padding: .5em;
+
+    img{
+      width: 1.5em;
+    }
+  }
+}
 </style>
