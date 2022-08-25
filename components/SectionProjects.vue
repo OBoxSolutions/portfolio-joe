260bf3084afde388ac88868830b7eb0cbfd80e44
Elodin <jcassola96@gmail.com>
2022-08-25 12:53:12 -0400

    create projects section structure

--- components/SectionProjects.vue
+++ components/SectionProjects.vue
@@ -1,15 +1,20 @@
 <template>
-  <div id="projects">
-    <div class="card" style="width: 18rem;">
-      <img class="card-img-top" src="https://via.placeholder.com/400" alt="Card image cap">
-      <div class="card-body">
-        <h5 class="card-title">
-          Card title
-        </h5>
-        <p class="card-text">
-          Some quick example text to build on the card title and make up the bulk of the card's content.
-        </p>
-        <base-button>Visit</base-button>
+  <div id="projects" class="bg-primary">
+    <h1>Projects</h1>
+    <div class="projects-container">
+      <div class="card bg-secondary" style="width: 18rem;">
+        <img class="card-img-top" src="https://via.placeholder.com/400" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title color-primary">
+            Card title
+          </h5>
+          <p class="card-text">
+            Some quick example text to build on the card title and make up the bulk of the card's content.
+          </p>
+          <base-button class="card-button">
+            Visit
+          </base-button>
+        </div>
       </div>
     </div>
   </div>
@@ -23,6 +28,13 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+#projects{
+  .card-button{
+    float: right;
+  }
+  .projects-container{
+    display: flex;
+  }
+}
 </style>
